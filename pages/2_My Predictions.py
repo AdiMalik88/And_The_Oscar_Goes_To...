ea6d6f699efc426369dffadf0395900b69f8c2ad
AdiMalik88 <156916974+AdiMalik88@users.noreply.github.com>
2024-03-20 10:15:30 +0000

    Update 2_My Predictions.py

--- pages/2_My Predictions.py
+++ pages/2_My Predictions.py
@@ -65,10 +65,20 @@ if __name__ == "__main__":
 # Add an image to the app
 def main():
     st.title("Two Important Critics (Me (AAM) & My Wife (AFC)) Predictions")
+    
+    image = 'Our Predictions.JPG'
+    st.image(image, caption='Two Important Critics', use_column_width=True)
+
+if __name__ == "__main__":
+    main()
+
+
+"""def main():
+    st.title("Two Important Critics (Me (AAM) & My Wife (AFC)) Predictions")
 
 image = 'Our Predictions.JPG'
 
 st.image(image, caption='Two Important Critics', use_column_width=True)
 
 if __name__ == "__main__":
-    main()
+    main()"""
